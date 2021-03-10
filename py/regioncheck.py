439e0279f2cb1e7edc2183e944685adea2ef76cb
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-10 02:25:11 -0800

    Peg Cave

--- py/regioncheck.py
+++ py/regioncheck.py
@@ -9,18 +9,18 @@ ids = []
 notids = []
 
 for filename in glob.iglob(root_dir + "**/*.json", recursive=True):
-		if "schema" not in filename and "caves" not in filename:
-				with(open(filename)) as json_file:
-						data = json.load(json_file)
-						region = filename.replace(os.path.join(".","regions",""),"")
-						region = region.replace(".json","")
-						if "rooms" in data:
-								for room in data["rooms"]:
-										if "id" in room:
-												ids.append(room["id"])
-						regions[region] = data
+    if "schema" not in filename and "cavess" not in filename:
+        with(open(filename)) as json_file:
+            data = json.load(json_file)
+            region = filename.replace(os.path.join(".","regions",""),"")
+            region = region.replace(".json","")
+            if "rooms" in data:
+                for room in data["rooms"]:
+                    if "id" in room:
+                        ids.append(room["id"])
+            regions[region] = data
 ids.sort()
 for i in range(1,ids[len(ids) - 1]):
-		if i not in ids:
-				notids.append(i)
+    if i not in ids:
+        notids.append(i)
 print(notids)
